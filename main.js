1a7fbaae42866b1a36f4cddf4c2987ca2040a23d
mangun <mngunawardana@gmail.com>
2015-11-05 22:08:27 +0530

    Add dependencies

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-var http = require("http");
-console.log("Hello World");
-http.createServer(function (request, response){
-   response.writeHead(200, {'Content-Type': 'text/plain'}); 
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(3000);
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
 
-console.log('Server running at http://localhost:3000/');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+   res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
